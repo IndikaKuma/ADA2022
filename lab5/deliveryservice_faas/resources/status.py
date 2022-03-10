26d018cd905e069d4dbdae63745d5f3a98ed29c7
IndikaKuma <ipkumarawd@yahoo.com>
2022-03-10 16:52:43 +0100

    str to int for big query

--- lab5/deliveryservice_faas/resources/status.py
+++ lab5/deliveryservice_faas/resources/status.py
@@ -1,6 +1,6 @@
 import datetime
 from flask import jsonify
-from daos.delivery_dao import StatusDAO
+from daos.delivery_dao import DeliveryDAO
 from db import Session
 
 
@@ -8,8 +8,8 @@ class Status:
     @staticmethod
     def update(d_id, status_text):
         session = Session()
-        statusobj = session.query(StatusDAO).filter(StatusDAO.id == int(d_id))[0]
-        statusobj.status = status_text
-        statusobj.last_update = datetime.datetime.now()
+        delivery = session.query(DeliveryDAO).filter(DeliveryDAO.id == int(d_id))[0]
+        delivery.status.status = status_text
+        delivery.status.last_update = datetime.datetime.now()
         session.commit()
         return jsonify({'message': 'The delivery status was updated'}), 200
