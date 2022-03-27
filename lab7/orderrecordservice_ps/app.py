f2ce33c1f3ec2a1fbe718bc9b45e7c6e690116d7
IndikaKuma <ipkumarawd@yahoo.com>
2022-03-27 17:30:42 +0200

    update lab 7

--- lab7/orderrecordservice_ps/app.py
+++ lab7/orderrecordservice_ps/app.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 orders = Orders()
 order = Order()
 project_id = os.environ['project_id']
-
+create_topic(project=project_id, topic="inventory_status")
 create_subscription(project=project_id, topic="inventory_status",
                     subscription="inventory_status_orderrecord_sub")
 create_topic(project=project_id, topic="order_status")
